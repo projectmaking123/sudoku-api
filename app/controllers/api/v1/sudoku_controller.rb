4f0f0a3482e190ae30ef2bb5cae467ddcd127e4a
projectmaking123 <jason.89.wang@gmail.com>
2017-07-09 16:13:55 -0400

    modified controller logic to take in generate

--- app/controllers/api/v1/sudoku_controller.rb
+++ app/controllers/api/v1/sudoku_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class SudokuController < ApplicationController
       def index
-        render json: {status: 'SUCCESS', message: 'solved puzzle', data: "Hello" }, status: :ok
+        render json: {status: 'SUCCESS', message: 'solved puzzle', data: "generate" }, status: :ok
       end
 
       def show
@@ -11,11 +11,11 @@ module Api
         if (params[:id]).split("").count == 81
           solved = puzzle.solve(params[:id])
           render json: {status: 'SUCCESS', message: 'solved puzzle', data: solved }, status: :ok
-        elsif (params[:id]).to_s == "solve"
+        elsif (params[:id]).to_s == "generate"
           solved = puzzle.solve("---------------------------------------------------------------------------------")
           render json: {status: 'SUCCESS', message: 'solved puzzle', data: solved }, status: :ok
         else
-          render json: {status: 'FAILED', message: 'Please input valid puzzle', data: nil }, status: :ok
+          render json: {status: 'FAILED', message: 'Please input valid puzzle', data: "invalid puzzle" }, status: :ok
         end
       end
     end
